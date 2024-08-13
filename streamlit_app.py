5a85e51df36f9088f94cd2e01905ff1916661afc
Vinishah Nagarajan <83562223+VinishahN@users.noreply.github.com>
2024-08-13 21:21:27 +0530

    Update streamlit_app.py
    Updated the code to get fruityvice data for every fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom smoothie!
@@ -7,11 +8,9 @@ st.write(
 )
 
 name_on_order = st.text_input("Name on smoothie: ");
-# st.write("The name on your smoothie will be: ", name_on_order);
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections=5);
 
@@ -19,13 +18,13 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order) values('""" + ingredients_string + """','""" + name_on_order+"""')"""
     time_to_insert = st.button("Submit Order");
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
